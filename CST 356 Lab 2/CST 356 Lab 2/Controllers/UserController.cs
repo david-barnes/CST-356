c4b7b5cdb4ce3775713ee93439b01a8741dedeca
david-barnes <barnesdz@yahoo.com>
2018-02-02 20:03:26 -0800

    CST356 Lab 2

---
+++ CST 356 Lab 2/CST 356 Lab 2/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CST_356_Lab_2.Data;
+using CST_356_Lab_2.Data.Entities;
+
+namespace CST_356_Lab_2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (user.FirstName == null || user.LastName == null || user.EmailAddress == null)
+                return RedirectToAction("Create");
+            user.Id = InMemoryDatabase.NextId();
+            InMemoryDatabase.Users.Add(user);
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            var users = InMemoryDatabase.Users;
+
+            return View(users);
+        }
+    }
+}
---
+++ CST 356 Lab 2/CST 356 Lab 2/Data/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace CST_356_Lab_2.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Display(Name = "Years in School")]
+        public int YearsInSchool { get; set; }
+
+    }
+}
