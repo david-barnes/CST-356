ec073f3e0e7fe6af2d1b219d1d071d92f5428e76
david-barnes <barnesdz@yahoo.com>
2018-02-24 15:31:47 -0800

    Lab 6 uploaded

---
+++ CST356 Lab 6/CST356 Lab 6/scripts/app.js
@@ -0,0 +1,15 @@
+﻿function displayInfo() {
+
+    alert("displaying");
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+
+    var summary = firstName + " " + lastName + " is " + age + " years old.";
+
+    document.getElementById("summary").innerText = summary;
+    
+    return false;
+}
